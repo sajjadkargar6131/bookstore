50f83d91e64991f6c59f5904fc7c23b5d3d0700d
sajjadkargar6131 <sajjad71kargar@gmail.com>
2024-11-13 17:13:35 +0330

    create static files and designe templages

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -156,3 +156,9 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 # Email config
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# static config
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
