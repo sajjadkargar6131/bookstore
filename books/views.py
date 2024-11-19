fd32798d340bdf59dad9159fb95725c83b9984e8
sajjadkargar6131 <sajjad71kargar@gmail.com>
2024-11-19 10:02:24 +0330

    update template

--- books/views.py
+++ books/views.py
@@ -6,6 +6,7 @@ from .forms import BookCreateForm
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 4
     template_name = 'books/book_list.html'
     context_object_name = 'books'
     
